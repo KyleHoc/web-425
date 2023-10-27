09e351166ca70414f0c897f078ad64fb1641f437
Kyle Hochdoerfer <kjhoch2@gmail.com>
2023-10-27 09:42:45 -0400

    Commenting added

--- week-2/my-app/src/app/app-routing.module.ts
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
+//Author: Kyle Hochdoerfer
+//Title: app-routing.module.ts
+//Date: 10/24/23
+//Description: Typescript my-app routing
+
+//Import modules and the Home, Contact, and about components
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 
+//Set up routing for the default page, as well as the Home, Contact, and About component pages
 const routes: Routes = [
   {
     path: '',
@@ -23,6 +30,7 @@ const routes: Routes = [
   }
 ];
 
+//Create and export the app routing module
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
